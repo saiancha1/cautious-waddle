cbb51c44c26614bb95d126d5694fffba40a07fe5
David Cockerill <davidcnz@gmail.com>
2018-08-18 17:05:47 -0400

    building on login authentication

--- ClientApp/src/components/Authentication/Login.js
+++ ClientApp/src/components/Authentication/Login.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import AuthService from './AuthService';
 
 class Login extends Component {
   constructor() {
     super();
     this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.Auth = AuthService();
+  }
+
+  componentWillMount() {
+    if (this.Auth.loggedIn()) {
+      this.props.history.replace('/');
+    }
   }
 
   handleChange(e) {
@@ -17,22 +26,24 @@ class Login extends Component {
   render() {
     return (
       <div>
-        <input
-          placeholder="Email"
-          name="userEmail"
-          type="text"
-          onChange={this.handleChange}
-        />
-        <input
-          placeholder="Password"
-          name="userPass"
-          type="password"
-          onChange={this.handleChange}
-        />
-        <input
-          value="LOGIN"
-          type="submit"
-        />
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            placeholder="Email"
+            name="userEmail"
+            type="text"
+            onChange={this.handleChange}
+          />
+          <input
+            placeholder="Password"
+            name="userPass"
+            type="password"
+            onChange={this.handleChange}
+          />
+          <input
+            value="LOGIN"
+            type="submit"
+          />
+        </form>
       </div>
     );
   }
