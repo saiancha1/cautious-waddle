90a8c8b8c2e71b925438a4b9f103ad1cfe26204a
jamie bennett <jamie.a.bennett@gmail.com>
2018-08-26 16:39:35 +0800

    Updated with  static JSX code for company list

--- ClientApp/src/components/SummerTech/SummerTech.js
+++ ClientApp/src/components/SummerTech/SummerTech.js
@@ -1,59 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./SummerTech.css";
 import Header from "../Header/Header.js"
-import Footer from "../Footer/Footer.js"
 
-const SummerTech = (props) => {
-  
-  return ( <div><Header title="Summer Of Tech"/>
-    <div class="jumbotron text-center">
-  <h1>Summer Of Tech</h1>
-  <p>Lorem ipsum dolor sit amet..</p>
-</div>
-  
-<div class="container-fluid">
- <div class="row">
-    <div class="col-sm-3">
-      <h4></h4>
+
+class SummerTech extends Component {
+
+render(){
+  return (
+    <div className="summer"> <Header />
+    <div className="jumbotron text-center">
+      <input className="pull-left" type="button" value="Login" onclick="msg()"/> 
+      <h1>Summer Of Tech</h1>
+      <p>Lorem ipsum dolor sit amet..</p>
     </div>
-    <div class="col-sm-6">
-     <ul class = "media-list">
-   <li class = "media">
-      <a class = "pull-left" href = "#">
-         <img class = "media-object" src = "/bootstrap/images/64.jpg" alt = "company Logo" />
-      </a>
-      
-      <div class = "media-body">
-         <h4 class = "media-heading">Company Name</h4>
-			     <p>
-            This is text that describes the company. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-          <a href="">More Info</a>
-          </div>
-        </li>
-     
+    <div className="container-fluid">
+      <div class="row">
+        <div class="col-sm-3">
+        <h4></h4>
+        </div>
+      <div class="col-sm-6">
+        <ul class = "media-list">
         <li class = "media">
-      <a class = "pull-left" href = "#">
-         <img class = "media-object" src = "/bootstrap/images/64.jpg" alt = "company Logo" />
-      </a>
-      
-      <div class = "media-body">
-         <h4 class = "media-heading">Company Name</h4>
-			     <p>
-            This is text that describes the company. Lorem ipsum dolor sit amet, consectetur        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-          <a href="">More Info</a>
+          <a class = "pull-left" href = "#">
+            <img class = "media-object" src = "/bootstrap/images/64.jpg" alt = "company Logo" ></img>
+          </a>
+  
+          <div class = "media-body">
+            <h4 class = "media-heading">Company Name</h4>
+                <p>
+              This is text that describes the company. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <a href="">More Info</a>
           </div>
-        </li>
-      </ul>
+          </li>
+ 
+    <li class = "media">
+  <a class = "pull-left" href = "#">
+     <img class = "media-object" src = "/bootstrap/images/64.jpg" alt = "company Logo" ></img>
+  </a>
+  
+  <div class = "media-body">
+     <h4 class = "media-heading">Company Name</h4>
+           <p>
+        This is text that describes the company. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+      <a href="">More Info</a>
       </div>
-    <div class="col-sm-3">
-      <h4></h4>
-    </div>
+    </li>
+  </ul>
   </div>
-  <br/><br/><br/><br/>
+      <div class="col-sm-3">
+      <h4></h4>
+      </div>
 </div>
 </div>
-)};
-
-export default SummerTech;
+</div>)
+}
+}
+  export default SummerTech;
