37d54941e48d3b8a1057fea74a3cada829965d70
Has <kakahass@gmail.com>
2018-09-22 14:18:29 -0700

    CP-85 Consultant Api Call complete, created separate consultant file

--- ClientApp/src/components/Consultants.js
+++ ClientApp/src/components/Consultants.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Consultants = () => (
-  <div>
-    <h1>Consultants</h1>
-  </div>
-);
+// const cons = console;
+
+class Consultants extends Component {
+  state = {
+    consultants: ['test', [1, 2, 3]],
+  }
+
+  async componentWillMount() {
+    try {
+      const res = await fetch('api/Consultants/getConsultants');
+      const consultantlist = await res.json();
+      console.log(consultantlist);
+      this.setState({
+        consultants: consultantlist 
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    // console.log(this.state.consultants);
+    return (
+      <div>
+        <h1>Consultants</h1>
+        {this.state.consultants.map((consultant) => {
+          return (
+            <div>
+            <h3>{consultant.consultantName}</h3> 
+            <p>{consultant.consultantDesc}</p>
+            <p>{consultant.city}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+// const Consultants = () => (
+//   <div>
+//     <h1>Consultants</h1>
+//   </div>
+// );
 
 export default Consultants;
