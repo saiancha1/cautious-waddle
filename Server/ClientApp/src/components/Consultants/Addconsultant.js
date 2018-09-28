66eb686f5fa4d2b895b040d97897d95f949971cb
Has <kakahass@gmail.com>
2018-09-28 16:19:49 -0700

    CP 86 - Page mounts based on whether User is logged in or not

--- Server/ClientApp/src/components/Consultants/Addconsultant.js
+++ Server/ClientApp/src/components/Consultants/Addconsultant.js
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
 import AuthService from '.././Authentication/AuthService';
-import Login from '.././Authentication/Login';
-
-
-// const ress = this.Auth.loggedIn;
 
 class Addconsultant extends Component {
-  
   constructor(props) {
     super(props);
-    this.Auth=new AuthService();
-    this.Logn= new Login();
+    this.Auth = new AuthService();
     this.state = {
       loginStatus: false,
     }
   }
 
-  //before mounting we change the state to current login status
+  // before mounting we change the state to current login status
 async componentWillMount() {
   try { 
-    // const statuslogin = this.Auth.loggedIn();
-    this.setState ( {loginStatus: this.Auth.loggedIn()},
+    this.setState ( {loginStatus: this.Auth.loggedIn() },
    )} catch(e){
 
    } }
+
 
   render() {
     return (
