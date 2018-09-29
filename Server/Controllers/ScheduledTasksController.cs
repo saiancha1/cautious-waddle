844750bef8ad43940b6c24e10456b2d432fb892d
millar-98 <millar9819@gmail.com>
2018-09-29 22:02:18 +1200

    ScheduledTasksController

--- Server/Controllers/ScheduledTasksController.cs
+++ Server/Controllers/ScheduledTasksController.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 using Hangfire;
 
 using cautious_waddle.Models;
+using cautious_waddle.ViewModels;
 using cautious_waddle.Helpers;
 
 namespace cautious_waddle.Controllers
@@ -15,21 +19,38 @@ namespace cautious_waddle.Controllers
     public class ScheduledTasksController : Controller
     {
         private IJobsRepository _jobsRepository;
-        public ScheduledTasksController(IJobsRepository jobsRepository)
+        private UserManager<AppUser> _userManager;
+        public ScheduledTasksController(IJobsRepository jobsRepository, UserManager<AppUser> userManager)
         {
             _jobsRepository = jobsRepository;
+            _userManager = userManager;
+        }
+
+        public async Task<IList<string>> GetUserRoles(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            var roles = await _userManager.GetRolesAsync(user);
+            return roles;
         }
 
         [HttpPost("addExpiredJobs")]
         [Authorize]
-        public IActionResult AddExpiredJobs()
+        public async Task<IActionResult> AddExpiredJobs()
         {
             try
             {
-                string UserId = IdentityHelper.GetUserId(HttpContext);
+                string userId = IdentityHelper.GetUserId(HttpContext);
+                var roles = await GetUserRoles(userId);
 
-                RecurringJob.AddOrUpdate<JobsRepository>(j => j.ExpiredJobs(), Cron.Minutely);
-                return Ok();
+                if(roles[0] == "Admin")
+                {
+                    RecurringJob.AddOrUpdate<JobsRepository>(j => j.ExpiredJobs(), Cron.Minutely);
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             catch(Exception ex)
             {
