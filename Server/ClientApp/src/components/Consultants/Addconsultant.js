b9ddb53af6225be39c24b28c65345fab0abe1d5a
Has <kakahass@gmail.com>
2018-09-29 21:18:18 -0700

    CP-86 Updated Add consultant +added new form. Updated moved contact pg

--- Server/ClientApp/src/components/Consultants/Addconsultant.js
+++ Server/ClientApp/src/components/Consultants/Addconsultant.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ConForm from './ConForm';
 import AuthService from '.././Authentication/AuthService';
 
 class Addconsultant extends Component {
@@ -6,32 +7,19 @@ class Addconsultant extends Component {
     super(props);
     this.Auth = new AuthService();
     this.state = {
-      loginStatus: false,
-    }
+      loginStatus: true,
+    };
   }
-
-  // before mounting we change the state to current login status
-async componentWillMount() {
-  try { 
-    this.setState ( {loginStatus: this.Auth.loggedIn() },
-   )} catch(e){
-
-   } }
-
 
   render() {
     return (
-  <div> {this.state.loginStatus ? (
-    <h1>logged in</h1>
-  ) : (<h1>logged out </h1>)
-  }
-    {/* {this.Auth.loggedIn ? (
-      <h1>Logged In</h1>
-    ) : (
-      <h1>Not logged in</h1>
-    )
-       } */}
-  </div>)};
+      <div> {this.state.loginStatus ? (
+      <ConForm />
+) : (<h1>logged out</h1>)
 }
+      </div>);
+  }
+}
+
 
 export default Addconsultant;
