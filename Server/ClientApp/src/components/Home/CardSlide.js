244f7731b5b6288ffd387bbe0de155b59a2c86a0
David Cockerill <davidcnz@gmail.com>
2018-10-02 21:11:05 -0400

    got map working with slick

--- Server/ClientApp/src/components/Home/CardSlide.js
+++ Server/ClientApp/src/components/Home/CardSlide.js
@@ -20,9 +20,32 @@ const events = [{
   info: 'Talk to a Robto',
   date: '7/2/18',
 },
+{
+  id: 4,
+  title: 'Meet tech man',
+  info: 'meet the man himself',
+  date: '3/9/18',
+}, {
+  id: 5,
+  title: 'Hot to give up Microsoft',
+  info: 'Learn how to stop using Microsoft',
+  date: '10/9/18',
+}, {
+  id: 6,
+  title: 'AI Today',
+  info: 'Talk to a Robto',
+  date: '7/2/18',
+},
 ];
 
 export default class CardSlide extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slides: [1, 2, 3],
+    };
+  }
+
   render() {
     const settings = {
       dots: true,
@@ -65,12 +88,44 @@ export default class CardSlide extends Component {
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         <h2>Upcoming Events</h2>
         <Slider className="slider-cards" {...settings}>
-          <div>
-            {events.map(event => (
-              <Card key={event.id} event={event} />
-            ))}
+          {/* <div>
+            <div>
+              {events.map(event => (
+                <Card key={event.id} event={event} />
+              ))}
+            </div>
+          </div> */}
+
+          {events.map(event => (
+            <div key={event.id}>
+              <Card event={event} />
+            </div>
+          ))}
+
+          {/* <div>
             <h3>1</h3>
           </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div> */}
+          {/* <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div> */}
         </Slider>
       </div>
     );
