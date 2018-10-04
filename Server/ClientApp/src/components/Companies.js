6a108039cb1ac6b58bd819503003dcaed2b7c035
David Cockerill <davidcnz@gmail.com>
2018-10-04 19:11:50 -0400

    resolve merge conflicts companies.js

--- Server/ClientApp/src/components/Companies.js
+++ Server/ClientApp/src/components/Companies.js
@@ -1,64 +1,46 @@
 import React, { Component } from 'react';
 import CompanyList from './Companies/CompanyList';
-<<<<<<< HEAD
-
-=======
 import CompanyView from './Companies/CompanyView';
->>>>>>> master
+
 class Companies extends Component {
   state = {
     companies: [],
-    selectedCompany:null,
-    companyOpen:false,
+    selectedCompany: null,
+    companyOpen: false,
   }
 
   async componentWillMount() {
     fetch('api/Companies/getCompanies').then(res => res.json())
-<<<<<<< HEAD
       .then((json) => {
         this.setState({ companies: json });
       });
   }
 
-=======
-    .then(json =>{
-      this.setState({companies:json});
-    })
-  
-    };
-  handleModalOpen = (company,e) => {
-    
-    this.setState({selectedCompany:company})
-    this.setState({companyOpen:true})
+  handleModalOpen = (company, e) => {
+    this.setState({ selectedCompany: company });
+    this.setState({ companyOpen: true });
     e.preventDefault();
   }
-  handleClose = () => {
-    this.setState({selectedCompany:null});
-    this.setState({companyOpen:false})
-  }
->>>>>>> master
-  render() {
-   let company;
-   if(this.state.selectedCompany !== null)
-   {
-     const selectedCompany = this.state.selectedCompany;
-    company =<CompanyView companyToRender={selectedCompany} handleClose={this.handleClose} companyOpen={this.state.companyOpen}/> ;
-    
-   }
-    return (
-<<<<<<< HEAD
-      <CompanyList companies={this.state.companies} />
 
-=======
+  handleClose = () => {
+    this.setState({ selectedCompany: null });
+    this.setState({ companyOpen: false });
+  }
+
+  render() {
+    let company;
+    if (this.state.selectedCompany !== null) {
+      const selectedCompany = this.state.selectedCompany;
+      company = <CompanyView companyToRender={selectedCompany} handleClose={this.handleClose} companyOpen={this.state.companyOpen} />;
+    }
+    return (
       <div>
-        
-        <CompanyList companies={this.state.companies} handleModalOpen={this.handleModalOpen}/>
+
+        <CompanyList companies={this.state.companies} handleModalOpen={this.handleModalOpen} />
         {company}
-      
+
       </div>
->>>>>>> master
     );
-    
   }
 
 
