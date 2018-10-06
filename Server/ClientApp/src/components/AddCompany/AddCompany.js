52a8a1f5a54ae388d346b4491abbae23ac372349
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-06 23:08:15 +0800

    Updated data bindings and functions

--- Server/ClientApp/src/components/AddCompany/AddCompany.js
+++ Server/ClientApp/src/components/AddCompany/AddCompany.js
@@ -9,21 +9,80 @@ class AddCompany extends Component {
       contactEmail: '',
       companyName: '',
       logo: '',
-      size:'',
-      businessType:'',
-      specialistArea:'',
-      companyDesc:'',
-      phone:'',
-      email:'',
-      address1:'',
-      address2:'',
-      suburb:'',
-      postalCode:'',
-      city:'',
-      country:'',
-      summerJobs:'',
+      size: '',
+      businessType: '',
+      specialistArea: '',
+      companyDesc: '',
+      phone: '',
+      email: '',
+      address1: '',
+      address2: '',
+      suburb: '',
+      postalCode: '',
+      city: '',
+      country: '',
+      summerJobs: '',
     };
   }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    const {
+      contactEmail,
+      companyName,
+      logo,
+      size,
+      businessType,
+      specialistArea,
+      companyDesc,
+      phone,
+      email,
+      address1,
+      address2,
+      suburb,
+      postalCode,
+      city,
+      country,
+      summerJobs,
+    } = this.state;
+
+    const res = () => fetch('api/companies/addCompany', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.Auth.getToken()}`,
+      },
+      body: JSON.stringify({
+        contactEmail: contactEmail,
+        companyName: companyName,
+        logo: logo,
+        size: size,
+        businessType: businessType,
+        specialistArea: specialistArea,
+        companyDesc: companyDesc,
+        phone: phone,
+        email: email,
+        address1: address1,
+        address2: address2,
+        suburb: suburb,
+        postalCode: postalCode,
+        city: city,
+        country: country,
+        summerJobs: summerJobs,
+      }),
+    });
+    const PostingCompany = () => res.JSON();
+    res();
+    console.log(PostingCompany);
+    console.log(res);
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
 
   postDataHandler = () => {
     const post = {
@@ -32,10 +91,6 @@ class AddCompany extends Component {
       location: this.state.location,
       description: this.state.description,
     };
-
-    // TODO: Change this for a fetch call instead of Axios
-
-    // Axios.post('https://capstone1.azurewebsites.net/api/companies/addCompany',post);
   }
 
   render() {
@@ -46,55 +101,72 @@ class AddCompany extends Component {
           <h1>Add Company</h1>
           <p>You may add your company listing using the below form</p>
           <p> Please complete all fields and then click submit.</p>
-          <form action={this.postDataHandler} formMethod="post">
+          <form onSubmit={this.handleSubmit}>
             <br />
                   Company Name:
-            <input type="text" name="companyName" />
+            <input name="companyName" value={this.state.companyName} onChange={this.handleChange} />
             <br />
             <br />
-                  Industry:
-            <input type="text" name="industry" />
+                  Logo:
+            <input name="logo" value={this.state.logo} onChange={this.handleChange} />
+            <br />
+            <br />
+                  Size:
+            <input name="size" value={this.state.size} onChange={this.handleChange} />
+            <br />
+            <br />
+                  Business Type:
+            <input name="businessType" value={this.state.businessType} onChange={this.handleChange} />
             <br />
             <br />
                 Speciality:
-            <input type="text" name="speciality" />
+            <input name="specialistArea" value={this.state.specialistArea} onChange={this.handleChange} />
+            <br />
+            <br />
+                Description:
+            <textarea name="companyDesc" value={this.state.companyDesc} onChange={this.handleChange} rows="10" cols="30" />
+            <br />
+            <br />
+                Telephone:
+            <input name="phone" value={this.state.phone} onChange={this.handleChange} />
             <br />
             <br />
                 Email:
-            <input type="text" name="email" />
+            <input name="email" value={this.state.email} onChange={this.handleChange} />
             <br />
             <br />
                 Website:
-            <input type="text" name="website" />
+            <input name="address1" value={this.state.address1} onChange={this.handleChange} />
             <br />
             <br />
                   Address:
-            <textarea name="message" rows="10" cols="30" />
+            <textarea name="address2" value={this.state.address2} onChange={this.handleChange} rows="10" cols="30" />
             <br />
             <br />
-                  Summary:
-            <textarea name="message" rows="10" cols="30" />
+                  Suburb:
+            <textarea name="suburb" value={this.state.suburb} onChange={this.handleChange} />
             <br />
             <br />
-                  Size (number of people):
-            <input type="text" name="size" />
+                  Postal Code:
+            <input name="postalCode" value={this.state.postalCode} onChange={this.handleChange} />
             <br />
             <br />
-                  Summer Students:
-            <select name="summerStudents">
+                  City:
+            <input name="city" value={this.state.city} onChange={this.handleChange} />
+            <br />
+            <br />
+                  Country:
+            <input name="country" value={this.state.country} onChange={this.handleChange} />
+            <br />
+            <br />
+                  Summer Jobs:
+            <select name="summerJobs" value={this.state.summerJobs} onChange={this.handleChange} >
               <option value="Yes">Yes</option>
               <option value="No">No</option>
             </select>
             <br />
             <br />
-                  Mailing List:
-            <select name="companyForm">
-              <option value="Yes">Yes</option>
-              <option value="No">No</option>
-            </select>
-            <br />
-            <br />
-            <input type="submit" value="Submit" />
+            <input id="submit" name="submit" type="submit" value="Submit" />
           </form>
         </div>
       );
