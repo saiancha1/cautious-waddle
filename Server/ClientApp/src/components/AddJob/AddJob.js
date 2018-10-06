5a119a74248e15c09d3e9253c6409ac18d88e35a
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-06 17:45:55 +0800

    Update to bring in sync with AddJobs on master branch

--- Server/ClientApp/src/components/AddJob/AddJob.js
+++ Server/ClientApp/src/components/AddJob/AddJob.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import AuthService from '../Authentication/AuthService';
+import { connect } from 'react-redux';
+//import AuthService from '../Authentication/AuthService';
 
 class AddJob extends Component {
-  state = {
-    loggedIn: AuthService.loggedIn(),
-    type: '',
-    position: '',
-    location: '',
-    description: '',
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      type: '',
+      position: '',
+      location: '',
+      description: '',
+    };
+
+    //this.Auth = new AuthService();
+  }
+
 
   postDataHandler = () => {
     const post = {
@@ -17,55 +24,68 @@ class AddJob extends Component {
       location: this.state.location,
       description: this.state.description,
     };
+
     // TODO: Change this for a fetch call instead of Axios
+
     // Axios.post('https://capstone1.azurewebsites.net/api/jobs/addJob',post);
   }
 
   render() {
-    if (this.state.loggedIn === true) {
-      return (
-      <div>
-              <h1>Add Job</h1>
-              <p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, 
-                mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id.
-{' '}
 
-            </p>
-              <form action="/action_page.php" formMethod="post">
-                  Type: 
-{' '}
-<select name="cars">
-                              <option value="Full Time">Full Time</option>
-                              <option value="Part Time">Part Time</option>
-                              <option value="Contract">Contract</option>
-                          </select>
-<br/>
-                  Position: 
-{' '}
-<input type="text" name="position"/>
-<br/>
-                  Location: 
-{' '}
-<input type="text" name="location"/>
-<br/>
-                  Description: 
-{' '}
-<textarea name="message" rows="10" cols="30"/>
-<br/>
-                  Image: 
-{' '}
-<input type="button" value="Upload" name="image"/>
-<br/>
-                <input type="submit" value="Submit" />
-              </form>
-            </div>
+    const login = this.props;
+    if (login.auth) {
+      return (
+        <div>
+          <h1>Add Job</h1>
+          <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec,
+                mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id.
+            {' '}
+
+          </p>
+          <form action={this.postDataHandler} formMethod="post">
+                  Type:
+            {' '}
+            <select name="cars">
+              <option value="Full Time">Full Time</option>
+              <option value="Part Time">Part Time</option>
+              <option value="Contract">Contract</option>
+            </select>
+            <br />
+                  Position:
+            {' '}
+            <input type="text" name="position" />
+            <br />
+                  Location:
+            {' '}
+            <input type="text" name="location" />
+            <br />
+                  Description:
+            {' '}
+            <textarea name="message" rows="10" cols="30" />
+            <br />
+                  Image:
+            {' '}
+            <input type="button" value="Upload" name="image" />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       );
     } else {
-          <div>
-                This page is accessible to valid account holders. If you have an account, please login to submit a new job posting.
-                </div>;
+      return (
+        <div>
+        This page is accessible to valid account holders. If you have an account, please login to submit a new job posting.
+        </div>);
     }
   }
 }
-export default AddJob;
+
+const mapStateToProps = state => (
+  {
+    auth: state.authenticated,
+  });
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddJob);
