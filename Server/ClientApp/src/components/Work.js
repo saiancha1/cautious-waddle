ee62deb6c9d8a52f67ae29b413247217b5fa6795
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-07 17:24:33 +0800

    started fetch calls to get job data from th backend

--- Server/ClientApp/src/components/Work.js
+++ Server/ClientApp/src/components/Work.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Work = () => (
-  <div>
-    <h1>Work</h1>
-  </div>
-);
+class Work extends Component {
+state = {
+  jobs: [],
+};
 
+async componentWillMount() {
+  fetch('api/jobs/getJobs').then(res => res.json())
+    .then((json) => {
+      this.setState({ jobs: json });
+    });
+}
+
+render() {
+  return (
+    <div className="jobs">
+      <h1>Work</h1>
+      <p>Lorem ipsum dolor sit amet..</p>
+      <ul>
+        {this.state.jobs.map(job => (
+                <li>
+                  <p>{job.jobTitle}</p>
+                </li>
+              ))}
+            </ul>
+    </div>
+  );
+}
+}
 export default Work;
