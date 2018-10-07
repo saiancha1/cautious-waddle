9dd6cca39cc8c61d5fb3b2c757413b42cbefd1cd
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-07 14:25:04 +0800

    Updates to data fields and post method

--- Server/ClientApp/src/components/AddJob/AddJob.js
+++ Server/ClientApp/src/components/AddJob/AddJob.js
@@ -1,88 +1,95 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import AuthService from '../Authentication/AuthService';
+import AuthService from '../Authentication/AuthService';
 
 class AddJob extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      type: '',
-      position: '',
-      location: '',
-      description: '',
-    };
-
-    //this.Auth = new AuthService();
-  }
-
-
-  postDataHandler = () => {
-    const post = {
-      type: this.state.type,
-      position: this.state.position,
-      location: this.state.location,
-      description: this.state.description,
-    };
-
-    // TODO: Change this for a fetch call instead of Axios
-
-    // Axios.post('https://capstone1.azurewebsites.net/api/jobs/addJob',post);
-  }
-
-  render() {
-    const login = this.props;
-    if (login.auth) {
-      return (
-        <div>
-          <h1>Add Job</h1>
-          <p>
-            To add a new job listing please complete and submit the below form.
-          </p>
-          <form action={this.postDataHandler} formMethod="post">
-                  Type:
-            {' '}
-            <select name="jobs">
-              <option value="Full Time">Full Time</option>
-              <option value="Part Time">Part Time</option>
-              <option value="Contract">Contract</option>
-            </select>
-            <br />
-                  Position:
-            {' '}
-            <input type="text" name="position" />
-            <br />
-                  Location:
-            {' '}
-            <input type="text" name="location" />
-            <br />
-                  Description:
-            {' '}
-            <textarea name="message" rows="10" cols="30" />
-            <br />
-                  Image:
-            {' '}
-            <input type="button" value="Upload" name="image" />
-            <br />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <p>This function is only available to valid account holders.</p>
-          <p>If you have an account, please login to submit a new job posting. If you do not have an account you can sign up via the login option above.</p>
-        </div>);
+    state = {
+      jobTitle: '',
+      jobDescription: '',
+      salary: '',
+      expiry: '',
     }
-  }
+
+    Auth = new AuthService();
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.state);
+      const {
+      jobTitleData,
+      jobDescriptionData,
+      salaryData,
+      expiryData,
+      } = this.state;
+
+      const res = () => fetch('/api/jobs/addJob', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.Auth.getToken()}`,
+        },
+        body: JSON.stringify({
+          jobTitle: 'Python Developer',
+          jobDescription: 'Machine Learning Python Development Role',
+          salary: 78000,
+          expiry: '12/1/2018 12:00:00',
+        }),
+      });
+      const PostingJobInfo = () => res.JSON();
+      res();
+      console.log(PostingJobInfo);
+      console.log(res);
+    }
+
+    handleChange = (e) => {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            {' '}
+                    Job Title
+          </label>
+          <br />
+          <input name="jobTitle" value={this.state.jobTitle} onChange={this.handleChange} required />
+          <br />
+          <br />
+          <label>
+                    Job Description
+          </label>
+          <br />
+          <input name="jobDescription" value={this.state.jobDescription} onChange={this.handleChange} required />
+          <br />
+          <br />
+          <label>
+                    Salary
+          </label>
+          <br />
+          <input name="salary" value={this.state.salary} onChange={this.handleChange} />
+          <br />
+          <br />
+          <label>
+                    Website
+          </label>
+          <br />
+          <input name="website" value={this.state.website} onChange={this.handleChange} />
+          <br />
+          <br />
+          <label>
+                    Expiry
+          </label>
+          <br />
+          <input name="expiry" type="datetime" value={this.state.expiry} onChange={this.handleChange} required />
+          <br />
+          {' '}
+          <br />
+          <input id="submit" name="submit" type="submit" value="Submit" />
+        </form>
+      );
+    }
 }
 
-const mapStateToProps = state => (
-  {
-    auth: state.authenticated,
-  });
-
-const mapDispatchToProps = dispatch => ({});
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddJob);
+export default AddJob;
