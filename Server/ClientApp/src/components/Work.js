4378a2803f922ede2ad4632f648cde35c4b802d7
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-07 18:11:34 +0800

    Job listings now working. Links to AddJob and SummerTech need work

--- Server/ClientApp/src/components/Work.js
+++ Server/ClientApp/src/components/Work.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Link } from 'react-router-dom';
 
 class Work extends Component {
 state = {
@@ -15,15 +16,28 @@ async componentWillMount() {
 render() {
   return (
     <div className="jobs">
+    <Link to="/summertech">
+          <button>Summer of Tech</button>
+          </Link>
+          <br/>
+          <br/>
+
       <h1>Work</h1>
       <p>Lorem ipsum dolor sit amet..</p>
       <ul>
         {this.state.jobs.map(job => (
                 <li>
-                  <p>{job.jobTitle}</p>
+                  <h1>{job.jobTitle}</h1>
+                  <p> Job Description: {job.jobDescription}</p>
+                  <p>Annual Salary: ${job.salary}</p>
                 </li>
               ))}
             </ul>
+            <br/>
+            <br/>
+          <Link to="/addJob">
+          <button>Add Job Listing</button>
+          </Link>
     </div>
   );
 }
