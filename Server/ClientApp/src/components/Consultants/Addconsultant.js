e4677dff25169ce662ec1aab3e8854935e901c7a
Has <kakahass@gmail.com>
2018-10-08 21:52:43 -0700

    CP 86 - deleted some unnecessary components

--- Server/ClientApp/src/components/Consultants/Addconsultant.js
+++ Server/ClientApp/src/components/Consultants/Addconsultant.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import ConForm from './ConForm';
 import AuthService from '../Authentication/AuthService';
-import Button from '@material-ui/core/Button';
 import StandardButton from './StandardButton';
-import NotloggedPop from './NotloggedPop';
 import SimplePopper from './Popper';
 import Popper from './Popper';
 import { Link } from 'react-router-dom'
