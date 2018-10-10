9f28200de0dc036f0300f6ad5c412b008f9d4d38
millar-98 <millar9819@gmail.com>
2018-10-10 17:49:17 +1300

    New columns added to localEvents

--- Server/ViewModels/Events/LocalEventsViewModel.cs
+++ Server/ViewModels/Events/LocalEventsViewModel.cs
@@ -13,14 +13,18 @@ namespace cautious_waddle.ViewModels
         public string EventName { get; set; }
         [JsonProperty("hostedBy")]
         public string HostedBy { get; set; }
+        [JsonProperty("contact")]
+        public string Contact { get; set; }
+        [JsonProperty("website")]
+        public string Website { get; set; }
         [JsonProperty("startDate")]
         public DateTime StartDate { get; set; }
-        [JsonProperty("duration")]
-        public int Duration { get; set; }
         [JsonProperty("recurring")]
         public int? Recurring { get; set; }
         [JsonProperty("eventDescription")]
         public string EventDescription { get; set; }
+        [JsonProperty("eventType")]
+        public string EventType { get; set; }
         [JsonProperty("eventLocation")]
         public string EventLocation { get; set; }
         [JsonProperty("imageURL")]
