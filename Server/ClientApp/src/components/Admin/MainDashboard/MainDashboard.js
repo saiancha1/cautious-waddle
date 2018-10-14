7e79b49c2139cc81dde82ad0cb188a2bf74131c2
Sai Ancha <sai.ancha@cin7.com>
2018-10-15 09:17:29 +1300

    remove react-moment

--- Server/ClientApp/src/components/Admin/MainDashboard/MainDashboard.js
+++ Server/ClientApp/src/components/Admin/MainDashboard/MainDashboard.js
@@ -3,7 +3,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
-import Moment from 'react-moment';
 import Analytics from './Analytics';
 import GeneralMetrics from './GeneralMetrics';
 import ExpiringSoon from './ExpiringSoon';
