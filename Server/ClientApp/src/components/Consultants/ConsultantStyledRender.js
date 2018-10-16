69350dcfaa7191392910862b5614bc6d7673fc59
Has <kakahass@gmail.com>
2018-10-15 18:14:25 -0700

    Committing my conditional render again!

--- Server/ClientApp/src/components/Consultants/ConsultantStyledRender.js
+++ Server/ClientApp/src/components/Consultants/ConsultantStyledRender.js
@@ -1,81 +1,107 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { connect } from 'react-redux';
 import AuthService from '../Authentication/AuthService';
 
-this.Auth = new AuthService();
 
 const styles = {
   card: {
     maxWidth: 345,
+    margin: 10,
   },
   media: {
     height: 350,
   },
 };
 
-this.handleDelete = (e) => {
-  const connerId = e.target.value;
-  console.log(connerId);
+class ConST extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  const res = () => fetch('api/Consultants/removeConsultant', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${this.Auth.getToken()}`,
-    },
-    body: connerId,
+  Auth = new AuthService();
+
+  handleDelete = (e) => {
+    const connerId = e.target.value;
+    console.log(connerId);
+
+    const res = () => fetch('api/Consultants/removeConsultant', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.Auth.getToken()}`,
+      },
+      body: connerId,
+    });
+      // .then(res.json());
+    res();
+  };
+
+
+  render() {
+    const { classes } = this.props;
+    return (
+      // console.log(status),
+      <div className="col-md-3">
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image={this.props.consultimage}
+            title={this.props.lastName}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h4">
+              {this.props.firstName}
+              {' '}
+              {this.props.lastName}
+              {' '}
+              {/* {props.conID}
+            {' '}
+            {props.nation} */}
+            </Typography>
+            <Typography gutterBottom variant="headline" component="h3">
+              {this.props.consultwebsite}
+            </Typography>
+            <br />
+            <Typography component="p" noWrap="true" paragraph="true">
+              {this.props.consultDescription}
+            </Typography>
+            <Typography>
+              {' '}
+            Based in
+              {' '}
+              {this.props.nation}
+              {' '}
+              {' '}
+
+            </Typography>
+            <Typography component="p" noWrap="true" paragraph="true">
+              {this.props.hisemail}
+            </Typography>
+          </CardContent>
+          <div>
+            {this.Auth.loggedIn() ? (<Button name="consultantId" value={this.props.conID} onClick={this.handleDelete}>DELETE</Button>) : (<div />) }
+          </div>
+        </Card>
+
+      </div>
+
+    );
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    auth: state.authenticated,
   });
-    // .then(res.json());
-  res();
-};
 
-const ConST = (props) => {
-  const { classes } = props;
-  return (
-    <div className="col-md-3">
-      <Card className={classes.card}>
-        <CardMedia
-          className={classes.media}
-          image={props.consultimage}
-          title={props.lastName}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h4">
-            {props.firstName}
-            {' '}
-            {props.lastName}
-            {' '}
-            {props.conID}
-            {' '}
-            {props.nation}
-          </Typography>
-          <Typography gutterBottom variant="headline" component="h2">
-            {props.consultwebsite}
-          </Typography>
-          <br />
-          <Typography component="p" noWrap="true" paragraph="true">
-            {props.consultDescription}
-          </Typography>
-          <Typography component="p" noWrap="true" paragraph="true">
-            {props.hisemail}
-          </Typography>
-        </CardContent>
-        <button name="consultantId" value={props.conID} onClick={this.handleDelete}>DELETE</button>
-      </Card>
-
-    </div>
-
-  );
-};
-
-ConST.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+const mapDispatchToProps = dispatch => ({});
 
 export default withStyles(styles)(ConST);
