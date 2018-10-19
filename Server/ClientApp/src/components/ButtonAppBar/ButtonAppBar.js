4b80f32ac25ba849e4287c92456f5d2b127d4005
jamie bennett <jamie.a.bennett@gmail.com>
2018-10-19 10:54:58 +0800

    Updates for jobs filter on toolbar

--- Server/ClientApp/src/components/ButtonAppBar/ButtonAppBar.js
+++ Server/ClientApp/src/components/ButtonAppBar/ButtonAppBar.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { BrowserRouter as Route, Router, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles, createMuiTheme } from '@material-ui/core/styles';
-import * as Colors from '@material-ui/core/colors';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
 import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
 import SearchIcon from '@material-ui/icons/Search';
 import AddJob from '../AddJob/AddJob';
 
@@ -37,42 +33,44 @@ function ButtonAppBar(props) {
   const handleChange = () => {
   };
 
-  const { classes, children } = props;
+  const { classes, children, filter, handleFilterChange } = props;
   console.log(children);
   return (
     <div className={classes.root}>
       <AppBar position="static" color="default">
         <Route exact path="/addjob" component={AddJob} />
         <Toolbar>
-            <InputLabel>
+          <FormControl>
+          <InputLabel>
             Type
-            </InputLabel>
-            <Input  margin="dense">
+          </InputLabel>
             <Select
+              value={filter}
               color="default"
-              onChange={handleChange}
+              onChange={handleFilterChange}
             >
-              <MenuItem value="" color='default'>
+              <MenuItem value="All" selected="true" color="default">
                 <em>Work Type</em>
               </MenuItem>
+              <MenuItem value="All">All</MenuItem>
               <MenuItem value="Full Time">Full Time</MenuItem>
               <MenuItem value="Part Time">Part Time</MenuItem>
               <MenuItem value="Contract">Contract</MenuItem>
             </Select>
-            </Input>
-            <Input 
+          </FormControl>
+          <Input
             margin="dense"
-              position="static"
-              startAdornment={(
-                <InputAdornment>
-                  <SearchIcon />
-                </InputAdornment>
+            position="static"
+            startAdornment={(
+              <InputAdornment>
+                <SearchIcon />
+              </InputAdornment>
               )}
-                color="default"
-                label="Search"
-                id="search-jobs"
-                variant="outlined"
-            />
+            color="default"
+            label="Search"
+            id="search-jobs"
+            variant="outlined"
+          />
           <Typography align="center" variant="h6" color="inherit" className={classes.grow}>
             {children}
           </Typography>
