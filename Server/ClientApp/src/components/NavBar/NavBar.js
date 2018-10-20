a2af52551e77a8e963a5ed3492b91b5f21ba9619
Your NameDavid <davidcnz@gmail.com>
2018-10-20 17:49:31 -0400

    make it responsive

--- Server/ClientApp/src/components/NavBar/NavBar.js
+++ Server/ClientApp/src/components/NavBar/NavBar.js
@@ -13,9 +13,9 @@ class NavBar extends Component {
     return (
       <div className="navbar-Wrapper">
         <ResponsiveMenu
-          menuOpenButton={<FontAwesomeIcon className="fa-icon" icon={faBars} />}
-          menuCloseButton={<FontAwesomeIcon className="fa-icon" icon={faTimes} />}
-          changeMenuOn="500px"
+          menuOpenButton={<FontAwesomeIcon className="nav-icon" icon={faBars} />}
+          menuCloseButton={<FontAwesomeIcon className="nav-icon" icon={faTimes} />}
+          changeMenuOn="700px"
           largeMenuClassName="large-menu"
           smallMenuClassName="small-menu"
           menu={(
@@ -29,41 +29,6 @@ class NavBar extends Component {
             </ul>
           )}
         />
-
-
-        {/* <Navbar fluid className="navbar-Wrapper">
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <div className="navItem-Wrapper">
-              <Nav>
-                <NavItem className="navItem" eventKey={1} componentClass={Link} href="/" to="/">
-              Home
-                </NavItem>
-                <NavItem className="navItem" eventKey={2} componentClass={Link} href="/companies" to="/companies">
-              Companies
-                </NavItem>
-                <NavItem className="navItem" eventKey={3} componentClass={Link} href="/consultants" to="/consultants">
-              Consultants
-                </NavItem>
-                <NavItem className="navItem" eventKey={4} componentClass={Link} href="/work" to="/work">
-              Work
-                </NavItem>
-                <NavItem className="navItem" eventKey={5} componentClass={Link} href="/events" to={{ pathname: '/events', state: { feature: 'none' } }}>
-              Events
-                </NavItem>
-                <NavItem className="navItem" eventKey={6} componentClass={Link} href="/contact" to="/contact">
-              Contact
-                </NavItem>
-                <NavItem className="navItem" eventKey={7} componentClass={Link} href="/subscribe" to="/subscribe">
-              Subscribe
-                </NavItem>
-                <NavItem className="navItem" eventKey={8} componentClass={Link} href="/summertech" to="/summertech">
-                SummerTech
-                </NavItem>
-              </Nav>
-            </div>
-          </Navbar.Collapse>
-        </Navbar> */}
       </div>
     );
   }
