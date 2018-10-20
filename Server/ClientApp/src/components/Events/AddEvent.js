490d14d3bc4d0102b72f89ea4067950d7e1c2c00
Your NameDavid <davidcnz@gmail.com>
2018-10-19 23:15:28 -0400

    styling form

--- Server/ClientApp/src/components/Events/AddEvent.js
+++ Server/ClientApp/src/components/Events/AddEvent.js
@@ -11,22 +11,81 @@ export default class AddEvent extends Component {
     super(props);
     this.Auth = new AuthService();
     this.state = {
-      eventName: '',
-      eventDescription: '',
-      eventLocation: '',
-      eventType: null,
-      startDate: new Date(),
-      startTime: '7:00',
-      contact: '',
-      website: null,
-      durationBool: false,
-      duration: 1,
-      recurring: null,
-      hostedBy: '',
+      event: {
+        eventName: '',
+        eventDescription: '',
+        eventLocation: '',
+        eventType: null,
+        startDate: new Date(),
+        startTime: '7:00',
+        contact: '',
+        website: null,
+        duration: 1,
+        hostedBy: '',
+        logo: '',
+      },
+      file: null,
+      imageUploaded: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleAddEventSubmit = this.handleAddEventSubmit.bind(this);
+    this.handleImageUpload = this.handleImageUpload.bind(this);
+  }
+
+  handleImageUpload = (e) => {
+    const data = new FormData();
+    const file = this.state.file;
+    data.append('file', file);
+    e.preventDefault();
+    fetch('api/events/addEventImage', { // Your POST endpoint
+      method: 'POST',
+      headers: {
+        // 'Content-Type': 'multipart/formdata',
+        Authorization: `Bearer ${localStorage.getItem('id_token')}`,
+      },
+      body: data, // This is your file object
+    }).then(
+      response => response.json(), // if the response is a JSON object
+    ).then((res) => {
+      const event = this.state.event;
+      event.logo = res.imageUrl;
+      this.setState({ event });
+      this.setState({ imageUploaded: true });
+      // Handle the success response object
+    },
+    ).catch(
+      error => console.log(error), // Handle the error response object
+    );
+  }
+
+  handleAddEventSubmit = () => {
+    const event = this.state.event;
+    if (event.eventId !== null) {
+      fetch('api/events/editevent', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('id_token')}`,
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+
+      })
+        .then((response) => { (response.status === 200) ? this.hide() : alert('fail1'); })
+        .catch();
+    } else {
+      fetch('api/events/addevent', {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('id_token')}`,
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify(event),
+
+      })
+        .then((response) => { (response.status === 200) ? this.setState({ redirect: true }) : alert('fail1'); })
+        .catch();
+    }
   }
 
   onChangeDate = date => this.setState({ startDate: date })
@@ -39,23 +98,18 @@ export default class AddEvent extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  handleSubmit(event) {
-    alert(`An essay was submitted: ${this.state.value}`);
-    event.preventDefault();
-  }
-
   render() {
     const {
-      eventName, eventDescription, eventLocation, eventType, durationBool,
-      startDate, startTime, contact, website, duration, recurring, hostedBy,
+      eventName, eventDescription, eventLocation, eventType,
+      startDate, startTime, contact, website, duration, hostedBy,
     } = this.state;
-
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <div className="event-form">
+        <form onSubmit={this.handleAddEventSubmit}>
           <label>Event Name</label>
           <div>
             <input
+              className="event-input"
               type="text"
               name="eventName"
               value={eventName}
@@ -66,6 +120,7 @@ export default class AddEvent extends Component {
           <label>Description</label>
           <div>
             <textarea
+              className="event-input"
               name="eventDescription"
               value={eventDescription}
               onChange={this.handleChange}
@@ -75,21 +130,74 @@ export default class AddEvent extends Component {
           <label>Hosted By</label>
           <div>
             <input
+              className="event-input"
               name="hostedBy"
               value={hostedBy}
               onChange={this.handleChange}
               required
             />
-          </div>       
+          </div>
           <label>Location</label>
           <div>
             <input
+              className="event-input"
               type="text"
               name="eventLocation"
               value={eventLocation}
               onChange={this.handleChange}
               required
             />
+          </div>
+          <label>Contact</label>
+          <div>
+            <textarea
+              className="event-input"
+              name="contact"
+              value={contact}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <label>Website</label>
+          <div>
+            <textarea
+              className="event-input"
+              name="website"
+              value={website}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div className="event-date-group">
+            <div className="time-picker">
+              <label>Time</label>
+              <TimePicker
+                onChange={this.onChangeTime}
+                value={startTime}
+                disableClock="true"
+                required
+              />
+            </div>
+            <div className="date-picker">
+              <label>Date</label>
+              <DatePicker
+                name="startDate"
+                onChange={this.onChangeDate}
+                value={startDate}
+                required
+              />
+            </div>
+            <div>
+              <label>Days</label>
+              <NumericInput
+                className="event-NumericInput"
+                min={0}
+                max={20}
+                name="duration"
+                value={duration}
+                onChange={this.onChangeDur}
+              />
+            </div>
           </div>
           <label>Type Of Event</label>
           <div>
@@ -105,64 +213,8 @@ export default class AddEvent extends Component {
               <option value="Course">Course</option>
             </select>
           </div>
-          <label>Date</label>
-          <div>
-            <DatePicker
-              name="startDate"
-              onChange={this.onChangeDate}
-              value={startDate}
-              required
-            />
-          </div>
-          <label>Contact</label>
-          <div>
-            <textarea
-              name="contact"
-              value={contact}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <label>Website</label>
-          <div>
-            <textarea
-              name="website"
-              value={website}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <label>Start Time</label>
-          <div>
-            <TimePicker
-              onChange={this.onChangeTime}
-              value={startTime}
-              disableClock="true"
-              required
-            />
-          </div>
-          {/* <label>Is The Event Longer Than One Day?</label>
-          <div>
-            <select
-              name="durationBool"
-              value={durationBool}
-              onChange={this.handleChange}
-              required
-            >
-              <option selected value={false}>No</option>
-              <option>Yes</option>
-            </select>
-          </div> */}
-          <label>Number Of Days</label>
-          <div>
-            <NumericInput
-              min={0}
-              max={20}
-              name="duration"
-              value={duration}
-              onChange={this.onChangeDur}
-            />
-          </div>
+          <input className="event-logo-upload" type="file" onChange={this.handleImageUpload} />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     );
