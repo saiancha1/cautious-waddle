b6fecf98fb07e2c5ed27e201ecd995f753d1e8d9
Your NameDavid <davidcnz@gmail.com>
2018-10-21 18:22:21 -0400

    finish responsive design

--- Server/ClientApp/src/components/Events.js
+++ Server/ClientApp/src/components/Events.js
@@ -23,7 +23,7 @@ export default class Events extends Component {
       events: [],
       eventsSelected: [],
       loggedIn: this.Auth.loggedIn(),
-      type: 'All',
+      type: 'all',
     };
 
     this.handleAddEvent = this.handleAddEvent.bind(this);
@@ -93,7 +93,7 @@ export default class Events extends Component {
         <div className="events-container">
           <div className="event-image-container">
             <img className="event-image" src={eventImage} alt="Hall filled with people" />
-            <div className="event-welcome" style={this.state.type === 'all' ? ({ top: '21%' }) : ({ top: '12%' })}>
+            <div className="event-welcome" style={this.state.type !== 'all' ? ({ top: '21%' }) : ({ top: '12%' })}>
               <h1>Events</h1>
               <h2>
               Find out whats happening in tech
