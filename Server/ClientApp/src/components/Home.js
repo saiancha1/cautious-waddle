7bc970555c6b93c07673351db575b0c1ad65825a
Your NameDavid <davidcnz@gmail.com>
2018-10-22 23:05:29 -0400

    add blurb

--- Server/ClientApp/src/components/Home.js
+++ Server/ClientApp/src/components/Home.js
@@ -25,6 +25,25 @@ class Home extends Component {
         </div>
         <div className="about-block">
           <h3 className="home-heading">About Us</h3>
+          <div className="about-blurb">
+            <hr />
+            <h4>
+            Tech Palmy aims to showcase and promote what the city has to offer when it comes to
+            local tech capabilities and events. As you’ll see from the website there are a number
+            of technology companies and tech-related events in Palmerston North already but we are
+            always keen to add more.
+              <br />
+              <br />
+              {' '}
+            If you’re in Palmerston North and have an IT/tech-related
+            business, or if you’re an IT consultant we’d love to have you listed on our site.
+              <br />
+              <br />
+            All you need to do is register via the login screen and add your company or add yourself
+            as a consultant.
+            </h4>
+            <hr />
+          </div>
           <Grid className="card-grid">
             <Row className="show-grid flip-cards">
               <Col xs={12} sm={6} md={4}>
